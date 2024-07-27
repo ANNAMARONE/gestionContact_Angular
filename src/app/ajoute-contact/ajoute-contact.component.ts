a8dd48c75c4271dbe293f255cbc1382bad637efe
ANNA MARONE <annamarone72@gmail.com>
2024-07-27 17:48:06 +0000

    modification d'un contact

--- src/app/ajoute-contact/ajoute-contact.component.ts
+++ src/app/ajoute-contact/ajoute-contact.component.ts
@@ -1,7 +1,8 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { contact } from './contact.model';
+import { error } from 'console';
 
 
 
@@ -13,14 +14,56 @@ import { contact } from './contact.model';
   templateUrl: './ajoute-contact.component.html',
   styleUrl: './ajoute-contact.component.scss'
 })
-export class AjouteContactComponent  {
+export class AjouteContactComponent implements OnInit {
 
 contacts: contact[]=[];
+corbeille: contact[] = [];
 utilisateur:contact = new contact();
+contactEnCours: contact | null = null;
+  indexEnCours: number | null = null;
+ngOnInit(): void {
+  const contactsString = localStorage.getItem('contacts');
+  if (contactsString) { 
+    this.contacts = JSON.parse(contactsString);
+  } else {
+    this.contacts = []; 
+  }
+}
+
+
   AjouterContact(): void{
     this.contacts.push(this.utilisateur);
     this.utilisateur = new contact();
-    localStorage.setItem('contacts',JSON.stringify(this.contacts))  }
+    localStorage.setItem('contacts',JSON.stringify(this.contacts)) 
+   }
+
+   SupprimerContact(contact:contact):void{
+const index = this.contacts.indexOf(contact);
+if(index !== -1){
+  const contactSupprime= this.contacts.splice(index,1)[0];
+  this.corbeille.push(contactSupprime);
+}
+   }
+   RestaurerContact(contact : contact):void{
+const index = this.corbeille.indexOf(contact);
+if(index !== -1){
+  const contactRestauré = this.corbeille.splice(index,1)[0];
+  this.contacts.push(contactRestauré);
+}
+   }
+
+   editContact(index : number):void{
+    this.contactEnCours = { ...this.contacts[index] };
+    this.indexEnCours = index;
+   }
+   ModifierContact():void{
+    if (this.indexEnCours !== null && this.contactEnCours) {
+      this.contacts[this.indexEnCours] = this.contactEnCours;
+      this.contactEnCours = null;
+      this.indexEnCours = null;
+    }
+  }
+   
 }
 
 
